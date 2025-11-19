2514d69d62a5a802b09596999717640a882a38f7
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-19 17:07:31 +0530

    server changes to connect mongoose

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes.js";
+
+// const __dirname = path.resolve();
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/blogapp";
+const PORT = process.env.PORT || 4000;
+dotenv.config();
+
+//Initialization of Express app
+const app = express();
+
+app.use(express.json());
+
+//Cors origin handling
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+//   res.json({ status: "200" }).sendFile(path.join(__dirname, "index.html"));
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Mongoose connected successfully!");
+    app.listen(PORT, () => {
+      console.log(`App running and listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error occured", err);
+  });
