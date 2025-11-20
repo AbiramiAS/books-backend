39bb623c8e5b45771fe5bcda7dd51ad5804618de
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-20 14:58:11 +0530

    Connect to DB configs updated

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import mongoose from "mongoose";
+import connectDB from "./configs/dbconnection.js"
 import corsOptions from "./configs/corsOptions.js";
 import userRoutes from "./routes/userRoutes.js";
 import booksRoute from "./routes/booksRoute.js";
 
-// const __dirname = path.resolve();
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/blog-app";
 const PORT = process.env.PORT || 4000;
 dotenv.config();
+connectDB();
 
 //Initialization of Express app
 const app = express();
@@ -24,17 +23,11 @@ app.use("/users", userRoutes);
 app.use("/books", booksRoute);
 
 app.get("/", (req, res) => {
-//   res.json({ status: "200" }).sendFile(path.join(__dirname, "index.html"));
+  res.json({ status: "200" }).sendFile(path.join(__dirname, "index.html"));
 });
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("Mongoose connected successfully!");
-    app.listen(PORT, () => {
-      console.log(`App running and listening on PORT ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Error occured", err);
-  });
+
+mongoose.connection.once("open", () => {
+  console.log("Connection to MongoDB is successful");
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+});
