0850da878956a2d6ad4aa2778973c4c07354c13a
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-20 16:08:51 +0530

    db connection configs updated

--- configs/dbconnection.js
+++ configs/dbconnection.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
+const DB_URL = process.env.MONGO_URI || "mongodb://localhost:27017/blog-app";
 const connectDB = async () => {
           try {
-                    await mongoose.connect(process.env.MONGO_URI);
+                    await mongoose.connect(DB_URL);
           }
           catch (err) {
                     console.log("Error Occured", err);
